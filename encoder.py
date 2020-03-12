c68085717b02569fa6316fea3108c17a33ff758f
Douglas Correa <dscorrea@inf.ufpel.edu.br>
2020-03-12 10:28:44 -0300

    adding encoder support to receive paths via command line and calling of+pc matlab encoder

--- encoder.py
+++ encoder.py
@@ -1,13 +1,24 @@
 import os
+import sys
 import matlab.engine
 
+args = {}
+
+for index, arg in enumerate(sys.argv):
+	if arg[0] == '-':
+		args[arg] = sys.argv[index + 1]
+
+print('\n----- ENCODER PARAMETERS')
+print('-Input: ' + args['-input'])
+print('-LF Type: ' + args['-type'])
+print('-Reference path: ' + args['-reference'])
+print('-Output path: ' + args['-output'])
+# for arg in sys.argv[1:len(sys.argv)]:
+# 	args[arg] = 
+
+# input_path = '/home/douglascorrea/light-field/dataset/lenslet/Bikes/'
+# reference_path = '/home/douglascorrea/light-field/dataset/black_border_central_sai/Bikes_central_border_qp22.ppm'
+# output_path = '/home/douglascorrea/teste/'
+
 matlab = matlab.engine.start_matlab()
-a = matlab.warp_SAIs('HDCA','the_stanford_bunny', 'blocks', 'median', nargout=0)
-
-# cmd_matlab = 'matlab -r '
-# cmd_add_path = "\"addpath('/home/douglascorrea/matlab-workspace/opticalFlow_PhaseCorrelation_encoder/'); " 
-# cmd_of_pc_encoder = "warp_SAIs('HDCA','the_stanford_bunny', 'blocks', 'median'); exit\""
-# cmd_call_of_pc_encoder = cmd_matlab + cmd_add_path + cmd_of_pc_encoder
-
-# print(cmd_call_of_pc_encoder)
-# os.system(cmd_call_of_pc_encoder)
+matlab.warp_SAIs(args['-type'], 'blocks', 'median', args['-input'], args['-reference'], args['-output'], nargout=0)
